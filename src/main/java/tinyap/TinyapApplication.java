08c9f4d4b2e13dac301efc28c52affa1f8ec2781
Christian Leskowsky <christian@leskowsky.net>
2024-01-08 06:33:43 -0500

    first

---
+++ src/main/java/tinyap/TinyapApplication.java
@@ -0,0 +1,13 @@
+package tinyap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyapApplication.class, args);
+    }
+
+}
