e42412e476cae9a85e3cf8f1f129e86c803b9643
Christian Leskowsky <christian@leskowsky.net>
2024-01-13 09:51:45 -0500

    Added index.html with form to find an actor

--- src/main/java/net/leskowsky/tinyap/TinyapApplication.java
+++ src/main/java/net/leskowsky/tinyap/TinyapApplication.java
@@ -1,4 +1,4 @@
-package tinyap;
+package net.leskowsky.tinyap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/net/leskowsky/tinyap/TinyapApplicationTests.java
+++ src/test/java/net/leskowsky/tinyap/TinyapApplicationTests.java
@@ -1,4 +1,4 @@
-package tinyap;
+package net.leskowsky.tinyap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
