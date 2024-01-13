1b4ab09b9915528233dd5fdff52e71f2062f5e62
Christian Leskowsky <christian@leskowsky.net>
2024-01-13 10:15:03 -0500

    Added very simple req logging filter

---
+++ src/main/java/net/leskowsky/tinyap/servlet/RequestLoggingFilter.java
@@ -0,0 +1,34 @@
+package net.leskowsky.tinyap.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class RequestLoggingFilter implements Filter {
+
+    public static Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        var startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        logger.info(String.format("path=%s method=%s status=%d duration_ms=%d",
+                req.getRequestURI(),
+                req.getMethod(),
+                resp.getStatus(),
+                System.currentTimeMillis() - startTime
+        ));
+    }
+}
